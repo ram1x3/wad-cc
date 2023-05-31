524ad7daa1c59260cd299a817e7093409077b850
ram1x3 <134673321+ram1x3@users.noreply.github.com>
2023-05-31 21:05:08 +0530

    Delete Cloud_Computing/GAE directory

--- Cloud_Computing/GAE/test.py
+++
@@ -1,6 +0,0 @@
-import webapp2
-
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.write("Hello World ! My name is Akash Patil !")
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
